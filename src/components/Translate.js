7eebb14d2727f1545a9dfe3134f15ff5a964c301
Jen Speak <jspeak6@gmail.com>
2021-01-03 20:56:15 -0800

    added Convert component

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Dropdown from './Dropdown';
+import Convert from './Convert';
 
 const options = [
     {
@@ -13,7 +14,15 @@ const options = [
     {
         label: 'Hindi',
         value: 'hi'
-    }
+    },
+    {
+        label: 'Spanish',
+        value: 'es'
+    },
+    {
+        label: 'Vietnamese',
+        value: 'vi'
+    },
 ];
 
 const Translate = () => {
@@ -34,6 +43,9 @@ const Translate = () => {
                 onSelectedChange={setLanguage}
                 options={options}
             />
+            <hr />
+            <h3 className="iu header">Output</h3>
+            <Convert text={text} language={language} />
         </div>
     );
 };
